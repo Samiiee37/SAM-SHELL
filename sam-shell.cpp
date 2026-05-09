7d3be1f59643165c4970bbc4ea23ebe8b2d31a70
Samarvir Singh <samarvirnitsri23@gmail.com>
2026-05-09 22:53:05 +0530

    add tokenization

--- sam-shell.cpp
+++ sam-shell.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 
 int main() {
 
   cout << "=====================\n";
-  cout << "   SAM SHELL v1.0\n";
+  cout << "   SAM SHELL v1.1\n";
   cout << "=====================\n";
   cout << "Welcome Samar!\n\n";
 
@@ -17,23 +20,41 @@ int main() {
     // show prompt
     cout << "myshell> ";
 
-    // read input
+    // read full line
     string command;
     getline(cin, command);
 
-    // convert string to char*
-    char *args[] = {const_cast<char *>(command.c_str()), NULL};
+    // split command into tokens
+    stringstream ss(command);
+
+    string token;
+
+    vector<char *> args;
+
+    while (ss >> token) {
+
+      args.push_back(strdup(token.c_str()));
+    }
+
+    // execvp needs NULL at end
+    args.push_back(NULL);
+
+    //safety check
+    if(args.size() == 1) {
+    continue;
+}
 
     // create child process
     pid_t pid = fork();
 
     if (pid == 0) {
 
-      // child process
-      execvp(args[0], args);
+      // child process executes command
+      execvp(args[0], args.data());
 
-      // exec only reaches here if failed
+      // only runs if exec fails
       cout << "Command failed\n";
+      exit(1);
     } else {
 
       // parent waits
